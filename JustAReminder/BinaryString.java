71df4837adf88ee71949a8f277afe7f61bdab830
Monika kumari <157510932+AdventuresMS@users.noreply.github.com>
2025-03-09 01:51:15 +0000

    commit

---
+++ JustAReminder/BinaryString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BinaryString{
+    public static void printString(String s, int n){
+        int m = s.length();
+        if(m==n){
+            System.out.println(s);
+            return;
+        }
+        if(m==0 ||s.charAt(m-1)=='0'){
+            printString(s+1, n);
+            printString(s+0,n);
+        }
+        else printString(s+0,n);
+    }
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter String:");
+    int n = sc.nextInt();
+    printString("",n);
+    sc.close();
+}
+}
