9d6baa8507f6b31c19fa65a4c4125a9b38d8a07b
Monika kumari <157510932+AdventuresMS@users.noreply.github.com>
2025-04-21 05:20:10 +0530

    StackImplementation.java

--- stack/StackImplementation.java
+++ stack/StackImplementation.java
@@ -1,1 +1,22 @@
+import java.util.Stack;
 
+public class BasicStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(17);
+        st.push(15);
+        st.push(19);
+        System.out.println(st);
+        System.out.println(st.peek());
+ System.out.println(st.pop());
+ System.out.println(st.pop());
+ System.out.println(st.size());
+ System.out.println((st));
+        while(st.size() > 1)
+{
+        st.pop();
+}
+System.out.println(st.peek());
+    }
+}
